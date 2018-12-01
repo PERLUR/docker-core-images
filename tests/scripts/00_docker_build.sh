52e60c6edf0ecfc567f94b7b68142e3c2c3aa4ad
Mark Stopka <mark.stopka@perlur.cloud>
2018-12-01 05:34:24 +0100

    Add Travis CI tests - Docker build tests

---
+++ tests/scripts/00_docker_build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+PROJECT_PREFIX="perlur"
+
+IMAGE_NAME="centos-base"
+BASE_NAME=${PROJECT_PREFIX}-${IMAGE_NAME}
+docker build -t ${BASE_NAME} ./${BASE_NAME}
+
+IMAGE_NAME="centos-nginx"
+BASE_NAME=${PROJECT_PREFIX}-${IMAGE_NAME}
+docker build -t ${BASE_NAME} ./${BASE_NAME}
+docker build -t perlur/centos-base ./centos-nginx\
+
+IMAGE_NAME="centos-nginx-php-fpm"
+BASE_NAME=${PROJECT_PREFIX}-${IMAGE_NAME}
+docker build -t ${BASE_NAME} ./${BASE_NAME}
