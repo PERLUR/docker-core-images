9683b8113d10273b952b7002ac7c89c486fda276
Mark Stopka <mark.stopka@perlur.cloud>
2018-12-01 05:45:33 +0100

    Fix bug in Docker build script

--- tests/scripts/00_docker_build.sh
+++ tests/scripts/00_docker_build.sh
@@ -4,13 +4,13 @@ PROJECT_PREFIX="perlur"
 
 IMAGE_NAME="centos-base"
 BASE_NAME=${PROJECT_PREFIX}-${IMAGE_NAME}
-docker build -t ${BASE_NAME} ./${BASE_NAME}
+docker build -t ${BASE_NAME} ./${IMAGE_NAME}
 
 IMAGE_NAME="centos-nginx"
 BASE_NAME=${PROJECT_PREFIX}-${IMAGE_NAME}
-docker build -t ${BASE_NAME} ./${BASE_NAME}
+docker build -t ${BASE_NAME} ./${IMAGE_NAME}
 docker build -t perlur/centos-base ./centos-nginx\
 
 IMAGE_NAME="centos-nginx-php-fpm"
 BASE_NAME=${PROJECT_PREFIX}-${IMAGE_NAME}
-docker build -t ${BASE_NAME} ./${BASE_NAME}
+docker build -t ${BASE_NAME} ./${IMAGE_NAME}
