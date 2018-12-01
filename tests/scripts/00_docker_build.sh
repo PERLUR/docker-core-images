1ad194d80cfd83f60ec8fb35eef49068e0778e2f
Mark Stopka <mark.stopka@perlur.cloud>
2018-12-01 06:05:27 +0100

    Fix bugs - Add executable flag to debian-odoo docker entrypoint script - Fix Docker tag issue with debian-odoo image

--- tests/scripts/00_docker_build.sh
+++ tests/scripts/00_docker_build.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-PROJECT_PREFIX="perlur"
+DOCKER_PROJECT_PREFIX="perlur"
 
 IMAGE_NAME="centos-base"
-BASE_NAME=${PROJECT_PREFIX}-${IMAGE_NAME}
+BASE_NAME=${DOCKER_PROJECT_PREFIX}-${IMAGE_NAME}
 docker build -t ${BASE_NAME} ./${IMAGE_NAME}
 
 IMAGE_NAME="centos-nginx"
-BASE_NAME=${PROJECT_PREFIX}-${IMAGE_NAME}
+BASE_NAME=${DOCKER_PROJECT_PREFIX}-${IMAGE_NAME}
 docker build -t ${BASE_NAME} ./${IMAGE_NAME}
 docker build -t perlur/centos-base ./centos-nginx\
 
 IMAGE_NAME="centos-nginx-php-fpm"
-BASE_NAME=${PROJECT_PREFIX}-${IMAGE_NAME}
+BASE_NAME=${DOCKER_PROJECT_PREFIX}-${IMAGE_NAME}
 docker build -t ${BASE_NAME} ./${IMAGE_NAME}
+
+IMAGE_NAME="centos-nginx-php-fpm"
+BASE_NAME=${DOCKER_PROJECT_PREFIX}-${IMAGE_NAME}
+docker build -t ${BASE_NAME} ./${IMAGE_NAME}
+
+IMAGE_NAME="debian-odoo"
+BASE_NAME=${DOCKER_PROJECT_PREFIX}-${IMAGE_NAME}
+DOCKER_IMAGE_TAG="12.0"
+docker build -t ${BASE_NAME}:${DOCKER_IMAGE_TAG} ./${IMAGE_NAME}/${DOCKER_IMAGE_TAG}
