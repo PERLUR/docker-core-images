82bd7efdf713616f307589ed3465dc936d0fb62b
Mark Stopka <mark.stopka@perlur.cloud>
2019-01-18 10:57:31 +0100

    Fix indent

--- centos-laraplate/usr/local/bin/docker-entrypoint.sh
+++ centos-laraplate/usr/local/bin/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ -d $LARAVEL_DIRECTORY ]; then
     # the directory exists
     if [ "$(ls -A $LARAVEL_DIRECTORY)" ]; then
       echo "Directory not empty!"
-			echo "Attempting to install Composer dependencies!"
+      echo "Attempting to install Composer dependencies!"
       composer install --prefer-dist
     else
       composer create-project --prefer-dist laravel/laravel ./
