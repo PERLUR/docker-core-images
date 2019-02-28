9a82ead64c4b1c6c54bfe2e3a861ac5f2a4469b7
Mark Stopka <mark.stopka@perlur.cloud>
2019-02-28 08:27:40 +0100

    Add CI environment handling

--- centos-laraplate/usr/local/bin/docker-entrypoint.sh
+++ centos-laraplate/usr/local/bin/docker-entrypoint.sh
@@ -5,6 +5,10 @@ if [ -d $LARAVEL_DIRECTORY ]; then
     # the directory exists
     if [ "$(ls -A $LARAVEL_DIRECTORY)" ]; then
       echo "Directory not empty!"
+      if [ $CI = true ];
+        echo "Running in CI environment, skipping installation of Composer dependencies!"
+        exec "$@"
+      fi
       echo "Attempting to install Composer dependencies!"
       composer self-update
       composer install --no-interaction --prefer-dist --no-dev
