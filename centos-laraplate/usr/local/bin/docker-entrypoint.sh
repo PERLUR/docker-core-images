eee0168a146dac8de45efede2fcaa9ef0fce4f7b
Mark Stopka <marekstopka@gmail.com>
2019-02-28 10:11:00 +0100

    Update centos-laraplate to support CI environment (#13)

--- centos-laraplate/usr/local/bin/docker-entrypoint.sh
+++ centos-laraplate/usr/local/bin/docker-entrypoint.sh
@@ -5,7 +5,12 @@ if [ -d $LARAVEL_DIRECTORY ]; then
     # the directory exists
     if [ "$(ls -A $LARAVEL_DIRECTORY)" ]; then
       echo "Directory not empty!"
+      if [ "$CI" == "true" ]; then
+        echo "Running in a CI environment; skipping installation of Composer dependencies!"
+        exec "$@"
+      fi
       echo "Attempting to install Composer dependencies!"
+      composer self-update
       composer install --no-interaction --prefer-dist --no-dev
     else
       composer create-project --prefer-dist laravel/laravel ./
