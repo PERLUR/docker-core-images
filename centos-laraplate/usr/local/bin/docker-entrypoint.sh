2ac79a451664794a16813faa8c8549c74563088a
Mark Stopka <mark.stopka@perlur.cloud>
2019-02-28 08:17:06 +0100

    Add composer self-update to docker-entrypoint.sh

--- centos-laraplate/usr/local/bin/docker-entrypoint.sh
+++ centos-laraplate/usr/local/bin/docker-entrypoint.sh
@@ -6,6 +6,7 @@ if [ -d $LARAVEL_DIRECTORY ]; then
     if [ "$(ls -A $LARAVEL_DIRECTORY)" ]; then
       echo "Directory not empty!"
       echo "Attempting to install Composer dependencies!"
+      composer self-update
       composer install --no-interaction --prefer-dist --no-dev
     else
       composer create-project --prefer-dist laravel/laravel ./
