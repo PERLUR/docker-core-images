bdfe680c5053ab5e441ca8fcdd9bfa7b3e92106b
Mark Stopka <mark.stopka@perlur.cloud>
2019-06-21 15:58:35 +0200

    Add CentOS n2n image (#20)

--- tests/scripts/00_docker_build.sh
+++ tests/scripts/00_docker_build.sh
@@ -6,10 +6,14 @@ IMAGE_NAME="centos-base"
 BASE_NAME=${DOCKER_PROJECT_PREFIX}-${IMAGE_NAME}
 docker build -t ${BASE_NAME} ./${IMAGE_NAME}
 
+IMAGE_NAME="centos-n2n"
+BASE_NAME=${DOCKER_PROJECT_PREFIX}-${IMAGE_NAME}
+docker build -t ${BASE_NAME} ./${IMAGE_NAME}
+
+
 IMAGE_NAME="centos-nginx"
 BASE_NAME=${DOCKER_PROJECT_PREFIX}-${IMAGE_NAME}
 docker build -t ${BASE_NAME} ./${IMAGE_NAME}
-docker build -t perlur/centos-base ./centos-nginx\
 
 IMAGE_NAME="centos-nginx-php-fpm"
 BASE_NAME=${DOCKER_PROJECT_PREFIX}-${IMAGE_NAME}
